0a42b54478e329e6a1ad053215d405c0c2d321f8
rnaveensrinivas <rnaveensrinivas@gmail.com>
2021-05-25 18:43:14 +0530

    This is Naveen

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){ 
 
-    printf("Hey I am Srini Mama and I am the gang(prank)ster of CEG"); 
+    printf("Hey I am Srini guy and I am the gang(prank)ster of CEG"); 
 
     return 0 ; 
 }
