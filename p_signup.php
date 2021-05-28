3e9576334ad2a2cde4a45f6571dfdd3444ea355e
Pavan Adithya <pavanadithyak.s@gmail.com>
2021-05-28 15:28:16 +0530

    inserrted p data

--- p_signup.php
+++ p_signup.php
@@ -1,17 +1,46 @@
 <?php
-$conn = mysqli_connect("localhost","root","","");
+$conn = mysqli_connect("localhost","root","","data_stethoscope");
+
+//error reporting 
+ini_set ("display_errors", "1");
+error_reporting(E_ALL);
+
+
 if(isset($_POST['submit']))
 {
     $pname = $_POST['pname'];
     $page = $_POST['page'];
-    $pid = $_POST['pid'];
-    $pemail = $_POST['pemial'];
+    $pemail = $_POST['pemail'];
     $pnum = $_POST['pnum'];
     $ppassword = $_POST['ppassword'];
     $pblood = $_POST['pblood'];
+    $filename = $_FILES['pid']['name'];
+    $tempname = $_FILES['pid']['tmp_name'];
+    $folder = "uploads/".$filename;
 
-    $query = "INSERT INTO Patient(name,phone) values('$name','$phone')";
+    $query = "INSERT INTO patient(pname,page,pid,pemail,pnum,ppassword,pblood) values('$pname','$page','$filename','$pemail','$pnum','$ppassword','$pblood')";
     $run = mysqli_query($conn,$query);
+    
+    //check query
+    if($run)
+    {
+        echo "sucess";
 
-    //HEllo
+    }
+    else
+    {
+        echo mysqli_error($conn);
+    }
+    
+    //move files
+    if(move_uploaded_file($tempname,$folder))
+    {
+        echo "Worked for Windows";
+    }
+    else
+    {
+        echo "Worked for Mac ;)";
+    }
+
 }
+?>
