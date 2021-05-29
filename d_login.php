41caf79635c1e243891cf58f643c30d404f6f0ef
Pavan Adithya <pavanadithyak.s@gmail.com>
2021-05-29 19:35:21 +0530

    dupload

--- d_login.php
+++ d_login.php
@@ -6,6 +6,9 @@ if(!$conn)
     echo "failed";
 
 }
+//error reporting 
+ini_set ("display_errors", "1");
+error_reporting(E_ALL);
 
 $dnumber = $_POST['dnum'];
 $dpwd = $_POST['dpwd'];
@@ -26,7 +29,28 @@ if($dpwd!=$password)
 }
 else
 {
-    echo "hello";
+    $tablename = "P_".$pnum;
+    echo $tablename;
+    $query1 = "SELECT * FROM $tablename";
+    $run1 = mysqli_query($conn, $query1);
+    if(!$run1)
+    {
+    echo mysqli_error($conn);
+    }
+    while($row = mysqli_fetch_assoc($run1)) 
+    {
+        echo "sno : {$row['sno']}<br>".
+            " DATE :{$row['date']}<br> ".
+            "filename : {$row['filename']} <br> ".
+           "dname :{$row['dname']}<br>". 
+           "dnum : {$row['dnum']}<br>";
+        $filename = "/Data-storage/uploads/patient/".$row['filename'];  
+        echo '<a href= "'.$filename.'" target="_blank"><button>Download</button></a>';
+     
+    } 
+    
+    echo "<a href= 'dupload.html' target='_blank'><button>Upload</button></a>";
+
 }
 
 
